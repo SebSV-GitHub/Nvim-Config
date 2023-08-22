5433f00de99ff48b8c2af0708dcb7b9b8cb76258
Sebastian Suarez Valencia <sebastian.suarez@limberhealth.com>
2023-08-21 23:14:53 -0500

    Fix colors

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,9 +1,11 @@
 local bufferline = require("bufferline")
 
+local c = require("vscode.colors").get_colors()
 bufferline.setup({
 	options = {
 		diagnostics = "nvim_lsp",
 		show_buffer_close_icons = false,
+		show_close_icon = false,
 		offsets = {
 			{
 				filetype = "NvimTree",
@@ -11,6 +13,21 @@ bufferline.setup({
 				text_align = "center",
 				separator = true,
 			},
+		},
+	},
+	highlights = {
+		background = {
+			fg = c.vscLineNumber,
+		},
+		buffer_visible = {
+			fg = c.vscLineNumber,
+		},
+		tab = {
+			fg = c.vscLineNumber,
+		},
+		tab_selected = {
+			fg = c.vscGray,
+			bold = true,
 		},
 	},
 })
